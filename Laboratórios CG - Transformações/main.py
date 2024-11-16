f3df983549dc038f7b3989b6c2aa5a38c649e19a
Francisco Welio <fwelio.dev@gmail.com>
2024-11-16 18:08:54 -0300

    Questão 1

--- Laboratórios CG - Transformações/main.py
+++ Laboratórios CG - Transformações/main.py
@@ -6,111 +6,272 @@ from OpenGL.GLU import *
 
 from opengl_frame import AppOgl
 
+def show_2d_buttons():
+    frame_left.pack()
+       # Exibe os botões 2D
+    frame_left3d.pack_forget()
+    frame_left1Q.pack_forget()
+  # Esconde os botões 3D
 
-def main():
-    root = tk.Tk()
-    root.geometry("1000x600")
-    root.configure(background="#000C66")
+def show_3d_buttons():
+    frame_left3d.pack()
 
-    # Frame para o lado esquerdo
-    frame_left = tk.Frame(root, width=150, height=600)
-    frame_left.configure(background="#000C66")
-    frame_left.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+    frame_left.pack_forget()
+    frame_left1Q.pack_forget()  # Esconde os botões 2D
 
-    # Frame intermediário1
-    frame_mid1 = tk.Frame(root, width=75, height=600)
-    frame_mid1.configure(background="#000C66")
-    frame_mid1.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+def show_1Q_buttons():
+    frame_left1Q.pack()
 
-    # Frame intermediário2
-    frame_mid2 = tk.Frame(root, width=75, height=600)
-    frame_mid2.configure(background="#000C66")
-    frame_mid2.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+    frame_left.pack_forget()
+    frame_left3d.pack_forget()
 
-    # Frame para o lado direito
-    frame_right = tk.Frame(root, width=700, height=600)
-    frame_right.pack(side=tk.RIGHT, fill=tk.BOTH, expand=False)
-    
-    # Adicionar o frame OpenGL ao lado direito
-    ogl_frame = AppOgl(frame_right, width=700, height=600)
-    ogl_frame.pack(fill=tk.BOTH, expand=False)  # Definindo expand=False para manter o tamanho fixo
+#
+root = tk.Tk()
+root.geometry("1000x600")
+root.configure(background="#000C66")
+#frame superior
+frame_top = tk.Frame(root, width=1200, height=20)
+frame_top.configure(background="#a0a0a0")
+frame_top.pack(side=tk.TOP, fill=tk.BOTH,expand=False)
+btn_top2d = tk.Button(frame_top, text="2D", command=show_2d_buttons)
+btn_top2d.grid(row=0, column=0, padx=5, pady=5)
+btn_top3d = tk.Button(frame_top, text="3D", command=show_3d_buttons)
+btn_top3d.grid(row=0, column=1, padx=5, pady=5)
 
-    ogl_frame.animate = 1
+btn_top1Q = tk.Button(frame_top, text="Questao 1", command=show_1Q_buttons)
+btn_top1Q.grid(row=0, column=2, padx=5, pady=5)
 
-    # Configuração de grid para alinhar os widgets
+btn_limapr = tk.Button(frame_top, text="Limpeza", command=lambda: ogl_frame.square_points(0))
+btn_limapr.grid(row=0, column=3, padx=5, pady=5)
 
-    # Caixa de entrada para o tamanho do quadrado
-    entry_tamanho = ctk.CTkEntry(frame_mid1, placeholder_text="size", height=10, width=40)
-    entry_tamanho.grid(row=0, column=0, padx=5, pady=5)
 
-    lbl_tamanho = tk.Label(frame_mid2, background="#000C66")
-    lbl_tamanho.grid(row=0, column=0, padx=5, pady=5)
+# Frame para o lado esquerdo
+frame_left = tk.Frame(root, width=300, height=600)
+frame_left.configure(background="#000C66")
+frame_left.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+# Frame para o lado esquerdo
+frame_left3d = tk.Frame(root, width=300, height=600)
+frame_left3d.configure(background="#000C66")
+frame_left3d.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
 
-    # Botão para desenhar um quadrado
-    btn_desenhar_quadrado = tk.Button(frame_left, text="Desenhar Quadrado", command=lambda: ogl_frame.square_points(int(entry_tamanho.get())))
-    btn_desenhar_quadrado.grid(row=1, column=0, padx=5, pady=5)
+# Frame para Questão 1
+frame_left1Q = tk.Frame(root, width=300, height=600)
+frame_left1Q.configure(background="#000C66")
+frame_left1Q.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+# Frame para o lado direito
+frame_right = tk.Frame(root)
+frame_right.pack(side=tk.RIGHT, fill=tk.BOTH, expand=False)
 
-    # Caixa de entrada para Fator de escala Sx
-    entry_sx = ctk.CTkEntry(frame_mid1, placeholder_text="sx", height=10, width=40)
-    entry_sx.grid(row=2, column=0, padx=5, pady=5)
+# Adicionar o frame OpenGL ao lado direito
+ogl_frame = AppOgl(frame_right, width=700, height=600)
+ogl_frame.pack(fill=tk.BOTH, expand=False)  # Definindo expand=False para manter o tamanho fixo
+ogl_frame.animate = 1
 
-    # Caixa de entrada para Fator de escala Sy
-    entry_sy = ctk.CTkEntry(frame_mid2, placeholder_text="sy", height=10, width=40)
-    entry_sy.grid(row=2, column=0, padx=6, pady=6)
 
-    # Botão para Escala
-    btn_scale = tk.Button(frame_left, text="Aplicar Escala", command=lambda: ogl_frame.escala(float(entry_sx.get()), float(entry_sy.get())))
-    btn_scale.grid(row=2, column=0, padx=5, pady=5)
+# Configuração de grid para alinhar os widgets
+#
+# Caixa de entrada para o tamanho do quadrado
+entry_tamanho = ctk.CTkEntry(frame_left, placeholder_text="size", height=10, width=40)
+entry_tamanho.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 
-    # Caixa de entrada para Translação Tx
-    entry_tx = ctk.CTkEntry(frame_mid1, placeholder_text="Tx", height=10, width=40)
-    entry_tx.grid(row=3, column=0, padx=5, pady=15)
+lbl_tamanho = tk.Label(frame_left, background="#000C66")
+lbl_tamanho.grid(row=9, column=1, padx=5, pady=5, sticky="nsew")
+lbl_tamanho2 = tk.Label(frame_left1Q, background="#000C66")
+lbl_tamanho2.grid(row=9, column=1, padx=5, pady=5, sticky="nsew")
+# Botão para desenhar um quadrado
+btn_desenhar_quadrado = tk.Button(frame_left, text="Desenhar Quadrado", command=lambda: ogl_frame.square_points(int(entry_tamanho.get())))
+btn_desenhar_quadrado.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
 
-    # Caixa de entrada para Translação Ty
-    entry_ty = ctk.CTkEntry(frame_mid2, placeholder_text="Ty", height=10, width=40)
-    entry_ty.grid(row=3, column=0, padx=5, pady=15)
+# Caixa de entrada para Fator de escala Sx
+entry_sx = ctk.CTkEntry(frame_left, placeholder_text="sx", height=10, width=40)
+entry_sx.grid(row=3, column=1, padx=5, pady=5, sticky="nsew")
 
-    # Botão para Translação
-    btn_translate = tk.Button(frame_left, text="Aplicar Translação", command=lambda: ogl_frame.translacao(int(entry_tx.get()), int(entry_ty.get())))
-    btn_translate.grid(row=3, column=0, padx=5, pady=5)
+# Caixa de entrada para Fator de escala Sy
+entry_sy = ctk.CTkEntry(frame_left, placeholder_text="sy", height=10, width=40)
+entry_sy.grid(row=3, column=2, padx=6, pady=6, sticky="nsew")
 
-    # Caixa de entrada para Rotacao
-    entry_rot = ctk.CTkEntry(frame_mid1, placeholder_text="ang", height=10, width=40)
-    entry_rot.grid(row=4, column=0, padx=5, pady=5)
+# Botão para Escala
+btn_scale = tk.Button(frame_left, text="Aplicar Escala", command=lambda: ogl_frame.escala(float(entry_sx.get()), float(entry_sy.get())))
+btn_scale.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
 
-    lbl_rot = tk.Label(frame_mid2, background="#000C66")
-    lbl_rot.grid(row=4, column=0, padx=5, pady=5)
+# Caixa de entrada para Translação Tx
+entry_tx = ctk.CTkEntry(frame_left, placeholder_text="Tx", height=10, width=40)
+entry_tx.grid(row=4, column=1, padx=5, pady=15, sticky="nsew")
 
-    # Botão para Rotação
-    btn_rotate = tk.Button(frame_left, text="Aplicar Rotação", command=lambda: ogl_frame.rotacao(int(entry_rot.get())))
-    btn_rotate.grid(row=4, column=0, padx=5, pady=5)
+# Caixa de entrada para Translação Ty
+entry_ty = ctk.CTkEntry(frame_left, placeholder_text="Ty", height=10, width=40)
+entry_ty.grid(row=4, column=2, padx=5, pady=15, sticky="nsew")
 
-    # Caixa de entrada para Fator A de cisalhamento
-    entry_a = ctk.CTkEntry(frame_mid1, placeholder_text="a", height=10, width=40)
-    entry_a.grid(row=5, column=0, padx=5, pady=5)
+# Botão para Translação
+btn_translate = tk.Button(frame_left, text="Aplicar Translação", command=lambda: ogl_frame.translacao(int(entry_tx.get()), int(entry_ty.get())))
+btn_translate.grid(row=4, column=0, padx=5, pady=5, sticky="nsew")
 
-    # Caixa de entrada para Fator B de cisalhamento
-    entry_b = ctk.CTkEntry(frame_mid2, placeholder_text="b", height=10, width=40)
-    entry_b.grid(row=5, column=0, padx=5, pady=5)
+# Caixa de entrada para Rotacao
+entry_rot = ctk.CTkEntry(frame_left, placeholder_text="ang", height=10, width=40)
+entry_rot.grid(row=5, column=1, padx=5, pady=5, sticky="nsew")
 
-    # Botão para Cisalhamento
-    btn_shear = tk.Button(frame_left, text="Aplicar Cisalhamento", command=lambda: ogl_frame.cisalhamento(int(entry_a.get()), int(entry_b.get())))
-    btn_shear.grid(row=5, column=0, padx=5, pady=5)
+lbl_rot = tk.Label(frame_left, background="#000C66")
+lbl_rot.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
 
-    # Botões para Reflexão
-    btn_refx = tk.Button(frame_mid1, text="Ref X", command=lambda: ogl_frame.reflexaoX())
-    btn_refx.grid(row=6, column=0, padx=5, pady=5)
+# Botão para Rotação
+btn_rotate = tk.Button(frame_left, text="Aplicar Rotação", command=lambda: ogl_frame.rotacao(int(entry_rot.get())))
+btn_rotate.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
 
-    btn_refy = tk.Button(frame_mid2, text="Ref Y", command=lambda: ogl_frame.reflexaoY())
-    btn_refy.grid(row=6, column=0, padx=5, pady=5)
+# Caixa de entrada para Fator A de cisalhamento
+entry_a = ctk.CTkEntry(frame_left, placeholder_text="a", height=10, width=40)
+entry_a.grid(row=6, column=1, padx=5, pady=5, sticky="nsew")
 
-    btn_reforigem = tk.Button(frame_mid1, text="Ref Origem", command=lambda: ogl_frame.reflexaoOrigem())
-    btn_reforigem.grid(row=7, column=0, padx=5, pady=6)
+# Caixa de entrada para Fator B de cisalhamento
+entry_b = ctk.CTkEntry(frame_left, placeholder_text="b", height=10, width=40)
+entry_b.grid(row=6, column=2, padx=5, pady=5, sticky="nsew")
 
-    btn_ref45 = tk.Button(frame_mid2, text="Ref Reta 45", command=lambda: ogl_frame.reflexao45())
-    btn_ref45.grid(row=7, column=0, padx=5, pady=7)
+# Botão para Cisalhamento
+btn_shear = tk.Button(frame_left, text="Aplicar Cisalhamento", command=lambda: ogl_frame.cisalhamento(int(entry_a.get()), int(entry_b.get())))
+btn_shear.grid(row=6, column=0, padx=5, pady=5, sticky="nsew")
 
-    root.mainloop()
+# Botões para Reflexão
+btn_refx = tk.Button(frame_left, text="Ref X", command=lambda: ogl_frame.reflexaoX())
+btn_refx.grid(row=7, column=1, padx=5, pady=5, sticky="nsew")
 
-if __name__ == '__main__':
-    main()
+btn_refy = tk.Button(frame_left, text="Ref Y", command=lambda: ogl_frame.reflexaoY())
+btn_refy.grid(row=7, column=2, padx=5, pady=5, sticky="nsew")
+
+btn_reforigem = tk.Button(frame_left, text="Ref Origem", command=lambda: ogl_frame.reflexaoOrigem())
+btn_reforigem.grid(row=8, column=1, padx=5, pady=6, sticky="nsew")
+
+btn_ref45 = tk.Button(frame_left, text="Ref Reta 45", command=lambda: ogl_frame.reflexao45())
+btn_ref45.grid(row=8, column=2, padx=5, pady=7, sticky="nsew")
+# COMEÇO 3d
+# Caixa de entrada para o tamanho do quadrado
+entry_tamanho_3d = ctk.CTkEntry(frame_left3d, placeholder_text="size", height=10, width=40)
+entry_tamanho_3d.grid(row=2, column=1, sticky="nsew")
+
+lbl_tamanho_3d = tk.Label(frame_left3d, background="#000C66")
+lbl_tamanho_3d.grid(row=9, column=0, padx=5, pady=5, sticky="nsew")
+
+# Botão para desenhar um quadrado
+btn_desenhar_quadrado_3d = tk.Button(frame_left3d, text="Desenhar Cubo", command=lambda: ogl_frame.square_points(int(entry_tamanho.get())))
+btn_desenhar_quadrado_3d.grid(row=2, column=0, padx=2, pady=2, sticky="nsew")
+
+# Caixa de entrada para Fator de escala Sx
+entry_sx_3d = ctk.CTkEntry(frame_left3d, placeholder_text="sx_3d", height=10, width=40)
+entry_sx_3d.grid(row=3, column=1, padx=2, pady=2, sticky="nsew")
+
+# Caixa de entrada para Fator de escala Sy
+entry_sy_3d = ctk.CTkEntry(frame_left3d, placeholder_text="sy_3d", height=10, width=40)
+entry_sy_3d.grid(row=4, column=0, padx=2, pady=2, sticky="nsew")
+# Caixa de entrada para Fator de escala Sz
+entry_sy_3d = ctk.CTkEntry(frame_left3d, placeholder_text="sz_3d", height=10, width=40)
+entry_sy_3d.grid(row=4, column=1, padx=6, pady=6, sticky="nsew")
+
+# Botão para Escala
+btn_scale_3d = tk.Button(frame_left3d, text="Aplicar Escala_3d", command=lambda: ogl_frame.escala(float(entry_sx.get()), float(entry_sy.get())))
+btn_scale_3d.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
+
+# Caixa de entrada para Translação Tx
+entry_tx_3d = ctk.CTkEntry(frame_left3d, placeholder_text="Tx_3d", height=10, width=40)
+entry_tx_3d.grid(row=5, column=1, padx=5, pady=15, sticky="nsew")
+
+# Caixa de entrada para Translação Ty
+entry_ty_3d = ctk.CTkEntry(frame_left3d, placeholder_text="Ty_3d", height=10, width=40)
+entry_ty_3d.grid(row=6, column=0, padx=5, pady=15, sticky="nsew")
+# Caixa de entrada para Translação Tz
+entry_ty_3d = ctk.CTkEntry(frame_left3d, placeholder_text="Tz_3d", height=10, width=40)
+entry_ty_3d.grid(row=6, column=1, padx=5, pady=15, sticky="nsew")
+
+# Botão para Translação
+btn_translate_3d = tk.Button(frame_left3d, text="Aplicar Translação_3d", command=lambda: ogl_frame.translacao(int(entry_tx.get()), int(entry_ty.get())))
+btn_translate_3d.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
+
+# Caixa de entrada para Rotacao
+entry_rot_3d = ctk.CTkEntry(frame_left3d, placeholder_text="ang_3dX", height=10, width=40)
+entry_rot_3d.grid(row=7, column=1, padx=5, pady=2, sticky="nsew")
+entry_rot_3d = ctk.CTkEntry(frame_left3d, placeholder_text="ang_3dY", height=10, width=40)
+entry_rot_3d.grid(row=8, column=0, padx=5, pady=2, sticky="nsew")
+entry_rot_3d = ctk.CTkEntry(frame_left3d, placeholder_text="ang_3dZ", height=10, width=40)
+entry_rot_3d.grid(row=8, column=1, padx=5, pady=2, sticky="nsew")
+
+lbl_rot_3d = tk.Label(frame_left3d, background="#000C66")
+lbl_rot_3d.grid(row=8, column=4, padx=5, pady=5, sticky="nsew")
+
+# Botão para Rotação
+btn_rotate_3d = tk.Button(frame_left3d, text="Aplicar Rotação_3d", command=lambda: ogl_frame.rotacao(int(entry_rot.get())))
+btn_rotate_3d.grid(row=7, column=0, padx=5, pady=5, sticky="nsew")
+
+# Caixa de entrada para Fator A de cisalhamento
+entry_a_3d = ctk.CTkEntry(frame_left3d, placeholder_text="a_3d", height=10, width=40)
+entry_a_3d.grid(row=9, column=1, padx=5, pady=5, sticky="nsew")
+
+# Caixa de entrada para Fator B de cisalhamento
+entry_b_3d = ctk.CTkEntry(frame_left3d, placeholder_text="b_3d", height=10, width=40)
+entry_b_3d.grid(row=10, column=0, padx=5, pady=5, sticky="nsew")
+# Caixa de entrada para Fator B de cisalhamento
+entry_b_3d = ctk.CTkEntry(frame_left3d, placeholder_text="c_3d", height=10, width=40)
+entry_b_3d.grid(row=10, column=1, padx=5, pady=5, sticky="nsew")
+
+# Botão para Cisalhamento
+btn_shear_3d = tk.Button(frame_left3d, text="Aplicar Cisalhamento_3d", command=lambda: ogl_frame.cisalhamento(int(entry_a.get()), int(entry_b.get())))
+btn_shear_3d.grid(row=9, column=0, padx=5, pady=5, sticky="nsew")
+
+# Botões para Reflexão
+btn_refx_3d = tk.Button(frame_left3d, text="Ref X_3d", command=lambda: ogl_frame.reflexaoX())
+btn_refx_3d.grid(row=11, column=1, padx=5, pady=5, sticky="nsew")
+
+btn_refy_3d = tk.Button(frame_left3d, text="Ref Y_3d", command=lambda: ogl_frame.reflexaoY())
+btn_refy_3d.grid(row=12, column=0, padx=5, pady=5, sticky="nsew")
+
+btn_refy_3d = tk.Button(frame_left3d, text="Ref Z_3d", command=lambda: ogl_frame.reflexaoY())
+btn_refy_3d.grid(row=12, column=1, padx=5, pady=5, sticky="nsew")
+
+btn_reforigem_3d = tk.Button(frame_left3d, text="Ref Origem_3d", command=lambda: ogl_frame.reflexaoOrigem())
+btn_reforigem_3d.grid(row=13, column=0, padx=5, pady=6, sticky="nsew")
+
+btn_ref45_3d = tk.Button(frame_left3d, text="Ref Reta 45_3d", command=lambda: ogl_frame.reflexao45())
+btn_ref45_3d.grid(row=13, column=1, padx=5, pady=7, sticky="nsew")
+
+
+## Questão 1
+##
+
+entry_X1Q = ctk.CTkEntry(frame_left1Q, placeholder_text="X Inicio", height=10, width=40)
+entry_X1Q.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
+entry_Y1Q = ctk.CTkEntry(frame_left1Q, placeholder_text="Y Inicio", height=10, width=40)
+entry_Y1Q.grid(row=2, column=2, padx=5, pady=5, sticky="nsew")
+entry_XEND_1Q = ctk.CTkEntry(frame_left1Q, placeholder_text="X Fim", height=10, width=40)
+entry_XEND_1Q.grid(row=2, column=3, padx=5, pady=5, sticky="nsew")
+entry_YEND_1Q = ctk.CTkEntry(frame_left1Q, placeholder_text="Y Fim", height=10, width=40)
+entry_YEND_1Q.grid(row=2, column=4, padx=5, pady=5, sticky="nsew")
+
+btn_DDA = tk.Button(frame_left1Q, text="Reta DDA", command=lambda: ogl_frame.DDA(int(entry_X1Q.get()), int(entry_Y1Q.get()), int(entry_XEND_1Q.get()), int(entry_YEND_1Q.get())))
+btn_DDA.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+
+
+
+btn_PM = tk.Button(frame_left1Q, text="Reta PM", command=lambda: ogl_frame.PontoMedio(float(entry_X1Q.get()), float(entry_Y1Q.get()), float(entry_XEND_1Q.get()), float(entry_YEND_1Q.get())))
+btn_PM.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
+
+
+entry_Circ = ctk.CTkEntry(frame_left1Q, placeholder_text="Raio", height=10, width=40)
+entry_Circ.grid(row=4, column=1, padx=5, pady=5, sticky="nsew")
+btn_CIRC_EXP = tk.Button(frame_left1Q, text="Circun Exp.", command=lambda: ogl_frame.linePoliExpli(int(entry_Circ.get())))
+btn_CIRC_EXP.grid(row=4, column=0, padx=5, pady=5, sticky="nsew")
+btn_CIRC_TRI = tk.Button(frame_left1Q, text="Circun TRI.", command=lambda: ogl_frame.linePoliTrig(int(entry_Circ.get())))
+btn_CIRC_TRI.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
+btn_CIRC_PM = tk.Button(frame_left1Q, text="Circun PM.", command=lambda: ogl_frame.linePoliPM(int(entry_Circ.get())))
+btn_CIRC_PM.grid(row=5, column=1, padx=5, pady=5, sticky="nsew")
+
+
+entry_R1_ELI = ctk.CTkEntry(frame_left1Q, placeholder_text="Eixo X", height=10, width=40)
+entry_R1_ELI.grid(row=6, column=1, padx=5, pady=5, sticky="nsew")
+entry_R2_ELI = ctk.CTkEntry(frame_left1Q, placeholder_text="Eixo Y", height=10, width=40)
+entry_R2_ELI.grid(row=6, column=2, padx=5, pady=5, sticky="nsew")
+entry_R3_ELI = ctk.CTkEntry(frame_left1Q, placeholder_text="Raio", height=10, width=40)
+entry_R3_ELI.grid(row=6, column=3, padx=5, pady=5, sticky="nsew")
+btn_CIRC_ELIP = tk.Button(frame_left1Q, text="Circun ELIP.", command=lambda: ogl_frame.linePoliElip(int(entry_R1_ELI.get()), int( entry_R2_ELI.get()),int( entry_R3_ELI.get()) ))
+btn_CIRC_ELIP.grid(row=6, column=0, padx=5, pady=5, sticky="nsew")
+
+show_2d_buttons()
+root.mainloop()
+
+#if __name__ == '__main__':
+   # main()
--- Laboratórios CG - Transformações/opengl_frame.py
+++ Laboratórios CG - Transformações/opengl_frame.py
@@ -12,7 +12,7 @@ from Transformações import Translacao
 from Transformações import Escala
 from Transformações import Cisalhamento
 from Transformações import Reflexao
-
+import math
 class AppOgl(OpenGLFrame):
     def initgl(self):
         """Inicializa o ambiente OpenGL"""
@@ -41,6 +41,19 @@ class AppOgl(OpenGLFrame):
 
         self.update()
 
+    def draw_scene3D(self):
+        """Redesenha a cena OpenGL para que os objetos etc. fiquem na tela"""
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.draw_axes(self.winfo_reqwidth(), self.winfo_reqheight()) #Desenhar eixos X e Y
+
+        # Desenha os pontos armazenados na lista
+        glBegin(GL_POINTS)
+        glColor3f(1.0, 0, 0)
+        for point in self.points:
+            glVertex2f(point[0], point[1])
+        glEnd()
+
+        self.update()
     def draw_axes(self, width, height): #Desenhar eixos X e Y 
         glBegin(GL_LINES)
         glColor3f(0.30, 0.30, 0.30)  # Cor para o eixo x
@@ -51,6 +64,18 @@ class AppOgl(OpenGLFrame):
         glVertex3f(0.0, height/2, 0.0)
         glEnd()
 
+    def draw_axes3d(self, width, height,depth): #Desenhar eixos X e Y 
+        glBegin(GL_LINES)
+        glColor3f(0.30, 0.30, 0.30)  # Cor para o eixo x
+        glVertex3f(-width/2, 0.0, 0.0)
+        glVertex3f(width/2, 0.0, 0.0)
+        glColor3f(0.30, 0.30, 0.30)  # Cor para o eixo y
+        glVertex3f(0.0, -height/2, 0.0)
+        glVertex3f(0.0, height/2, 0.0)
+        glVertex3f(0.0, 0.0, -depth/2)
+        glVertex3f(0.0, 0.0, depth/2)
+        glEnd()
+
     def draw_pixel(self, dc_x, dc_y):
         glBegin(GL_POINTS)
         glColor3f(1.0, 1.0, 1.0)
@@ -58,6 +83,7 @@ class AppOgl(OpenGLFrame):
         glEnd()
 
     def DDA(self, x0, y0, xEnd, yEnd):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         dx = xEnd - x0
         dy = yEnd - y0
         steps = max(abs(dx), abs(dy))
@@ -72,24 +98,140 @@ class AppOgl(OpenGLFrame):
             self.points.append((round(x), round(y)))
             #self.draw_pixel(round(x), round(y))
     
+    def PontoMedio(self, x0, y0, xEnd, yEnd):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        dx = abs(xEnd - x0)
+        dy = abs(yEnd - y0)
+        p = 2 * dy - dx
+        twoDy = 2 * dy
+        twoDyMinusDx = 2 * (dy - dx)
+        x, y = 0, 0
 
-    #Método para desenhar quadrado na origem
+        # Verifica se a linha está no primeiro oitante
+        if dy > dx:
+            print("Reta não se situa no 1º oitante")
+            return
+        
+        if x0 > xEnd:
+            x, y = xEnd, yEnd
+            xEnd = x0
+        else:
+            x, y = x0, y0
+            
+        self.points.append((round(x), round(y)))
+        
+        while x < xEnd:
+            x += 1
+            if p < 0:
+                p += twoDy
+            else:
+                y += 1
+                p += twoDyMinusDx
+            self.points.append((round(x), round(y)))
+        self.update()
+
+    # Método para desenhar quadrado com o ponto inferior esquerdo na origem
     def square_points(self, size):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Quando desenhar novo quadrado, limpa a tela antes
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Limpa a tela antes de desenhar o quadrado
         self.points = []
         self.square_points_list = []
 
-        x = round(size)/2
-        y = round(size)/2
-        self.DDA(-x, y, x, y)
-        self.DDA(x, y, x, -y)
-        self.DDA(x, -y, -x, -y)
-        self.DDA(-x, -y, -x, y)
+        # Define os pontos do quadrado
+        x = size
+        y = size
 
-        self.square_points_list = [(-x,y), (x,y), (x,-y), (-x,-y)]
+        # Desenha o quadrado com o vértice inferior esquerdo na origem
+        self.DDA(0, 0, x, 0)        # Linha inferior
+        self.DDA(x, 0, x, y)        # Linha direita
+        self.DDA(x, y, 0, y)        # Linha superior
+        self.DDA(0, y, 0, 0)        # Linha esquerda
 
-        return (-x,y),(x,y),(x,-y),(-x,-y)
-    
+        # Lista de pontos do quadrado com ponto inferior esquerdo na origem
+        self.square_points_list = [(0, 0), (x, 0), (x, y), (0, y)]
+
+        return (0, 0), (x, 0), (x, y), (0, y)
+
+    def linePoliExpli(self, r):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.redraw()
+        x=0
+        xEnd = r/math.sqrt(2)
+        RR = r**2
+        while(x<=xEnd):
+            XX = x**2
+            y = math.sqrt(RR-XX)
+            self.pontosCircunferencia(x,y)
+            x = x+1
+    def linePoliTrig(self, r):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.redraw()
+        theta = 0
+        thetaEnd = 45
+        while(theta<=thetaEnd):
+            x = r * math.cos(theta)
+            y = r * math.sin(theta)
+            self.pontosCircunferencia(x,y)
+            theta = theta+ 0.3
+
+    def linePoliPM(self, r):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.redraw()
+        x = 0
+        y = r
+        d = (5/4)-r
+        #d = (1-r)
+        self.pontosCircunferencia(x,y)
+        while(y>x):
+                if(d<0):
+                    d = d +(2*x +3)
+                else:
+                    d = d+ (2* (x-y)+5)
+                    y = y-1
+                x = x+1
+                self.pontosCircunferencia(x,y)
+
+    def linePoliElip(self, rx, ry,centro):
+        x = 0
+        y = ry
+        # Inicializa o parâmetro de decisão para a Região 1
+        d1 = (ry**2) - (rx**2 * ry) + (0.25 * rx**2)
+        dx = 2 * (ry**2) * x
+        dy = 2 * (rx**2) * y
+
+        # Desenha os pontos na Região 1
+        self.pontosElipse(x, y,centro)
+        while dx < dy:
+            if d1 < 0:
+                x += 1
+                dx += 2 * (ry**2)
+                d1 += dx + (ry**2)
+            else:
+                x += 1
+                y -= 1
+                dx += 2 * (ry**2)
+                dy -= 2 * (rx**2)
+                d1 += dx - dy + (ry**2)
+            self.pontosElipse(x, y,centro)
+
+    def pontosElipse(self, x, y,centro):
+        cx= centro
+        cy = centro  # O centro da elipse
+        self.points.append((cx + x, cy + y))
+        self.points.append((cx - x, cy + y))
+        self.points.append((cx + x, cy - y))
+        self.points.append((cx - x, cy - y))
+
+    def pontosCircunferencia(self, x, y):
+        self.points.append((round(x), round(y)))
+        self.points.append((round(x), round(-y)))
+        self.points.append((round(-x), round(y)))
+        self.points.append((round(-x), round(-y)))
+        self.points.append((round(y), round(x)))
+        self.points.append((round(y), round(-x)))
+        self.points.append((round(-y), round(x)))
+        self.points.append((round(-y), round(-x)))
+
+
     #Método para desenhar quadrado após a transformação
     def draw_square(self, point1, point2, point3, point4):
         self.DDA(point1[0], point1[1], point2[0], point2[1])
@@ -178,8 +320,13 @@ class AppOgl(OpenGLFrame):
 
         #Desenha o novo quadrado rotacionado
         self.draw_square(*self.square_points_list)
-
-
+    
+    #def RetaPM(self):
+    def draw_cube(self, point1, point2, point3, point4,point5, point6, point7, point8):
+        self.DDA(point1[0], point1[1], point2[0], point2[1])
+        self.DDA(point2[0], point2[1], point3[0], point3[1])
+        self.DDA(point3[0], point3[1], point4[0], point4[1])
+        self.DDA(point4[0], point4[1], point1[0], point1[1])
         
         
         
